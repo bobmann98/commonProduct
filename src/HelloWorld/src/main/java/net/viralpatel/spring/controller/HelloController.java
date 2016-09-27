799748166240674bcfceaa9a835300943657d91d
bobmann <bobmann98@gmail.com>
2016-09-27 18:26:17 +0900

    아직도 테스트

---
+++ src/HelloWorld/src/main/java/net/viralpatel/spring/controller/HelloController.java
@@ -0,0 +1,25 @@
+package net.viralpatel.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+
+		model.addAttribute("name", "John Doe");
+
+		return "welcome";
+	}
+	
+	@GetMapping("/post")
+	public String post(Model model) {
+
+		//model.addAttribute("name", "John Doe");
+
+		return "postTest_Information";
+	}
+}
